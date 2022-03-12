0f0c3a4387fa91fb7d18bac5fabf0a45bbc83416
Mikael Kalms <mikael@kalms.org>
2022-03-12 06:24:01 +0100

    Testcases for 1x1_8_c5_030, 040, gen

--- normal/c2p1x1_8_c5_040_test.c
+++ normal/c2p1x1_8_c5_040_test.c
@@ -2,14 +2,14 @@
 
 #include "testdata.h"
 #include "assert_array.h"
-#include "c2p1x1_8_c5_030.h"
+#include "c2p1x1_8_c5_040.h"
 
 #include <string.h>
 
 struct utest_state_s;
 extern struct utest_state_s utest_state;
 
-UTEST(normal, c2p1x1_8_c5_030) {
+UTEST(normal, c2p1x1_8_c5_040) {
 
 	const int chunkyx = 320;
 	const int chunkyy = 256;
@@ -18,8 +18,8 @@ UTEST(normal, c2p1x1_8_c5_030) {
 
 	memset(tempbuf, 0, chunkyx * chunkyy);
 
-	c2p1x1_8_c5_030_smcinit(chunkyx, chunkyy, scroffsy, bplsize);
-	c2p1x1_8_c5_030(random_320x256x8bpl_chunky, tempbuf);
+	c2p1x1_8_c5_040_init(chunkyx, chunkyy, scroffsy, bplsize);
+	c2p1x1_8_c5_040(random_320x256x8bpl_chunky, tempbuf);
 
 	ASSERT_ARRAY_EQ(random_320x256x8bpl_planar, tempbuf, chunkyx * chunkyy);
 }
