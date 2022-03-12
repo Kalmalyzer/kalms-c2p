9df2432c11756627a9cd46607fa650abe72a791e
Mikael Kalms <mikael@kalms.org>
2022-03-12 06:28:18 +0100

    Added testcase for c2p1x1_8_c5_030_2

---
+++ normal/c2p1x1_8_c5_030_2_test.c
@@ -0,0 +1,25 @@
+#include "../utest/utest.h"
+
+#include "testdata.h"
+#include "assert_array.h"
+#include "c2p1x1_8_c5_030_2.h"
+
+#include <string.h>
+
+struct utest_state_s;
+extern struct utest_state_s utest_state;
+
+UTEST(normal, c2p1x1_8_c5_030_2) {
+
+	const int chunkyx = 320;
+	const int chunkyy = 256;
+	const int scroffsy = 0;
+	const int bplsize = 320 * 256 / 8;
+
+	memset(tempbuf, 0, chunkyx * chunkyy);
+
+	c2p1x1_8_c5_030_2_smcinit(chunkyx, chunkyy, scroffsy, bplsize);
+	c2p1x1_8_c5_030_2(random_320x256x8bpl_chunky, tempbuf);
+
+	ASSERT_ARRAY_EQ(random_320x256x8bpl_planar, tempbuf, chunkyx * chunkyy);
+}
