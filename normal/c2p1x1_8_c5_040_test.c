466adcd56de930ca4fb7e060fc48a97822056119
Mikael Kalms <mikael@kalms.org>
2022-03-12 08:09:13 +0100

    Conformity

--- normal/c2p1x1_8_c5_040_test.c
+++ normal/c2p1x1_8_c5_040_test.c
@@ -15,11 +15,12 @@ UTEST(normal, c2p1x1_8_c5_040) {
 	const int chunkyy = 256;
 	const int scroffsy = 0;
 	const int bplsize = 320 * 256 / 8;
+	const int depth = 8;
 
-	memset(tempbuf, 0, chunkyx * chunkyy);
+	memset(tempbuf, 0, bplsize * depth);
 
 	c2p1x1_8_c5_040_init(chunkyx, chunkyy, scroffsy, bplsize);
 	c2p1x1_8_c5_040(random_320x256x8bpl_chunky, tempbuf);
 
-	ASSERT_ARRAY_EQ(random_320x256x8bpl_planar, tempbuf, chunkyx * chunkyy);
+	ASSERT_ARRAY_EQ(random_320x256x8bpl_planar, tempbuf, bplsize * depth);
 }
